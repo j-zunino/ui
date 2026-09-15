516a2a445ed9ffcec8334bdb02ea938a2a829cd4
j-zunino <zuninojuanbautista@gmail.com>
2026-09-14 23:35:15 -0300

    feat(web): update Home component layout

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,3 +1,5 @@
+import { Button } from "@/components/ui/button";
+import { IconBrandGithub } from "@tabler/icons-react";
 import { createRoute } from "@tanstack/react-router";
 import { rootRoute } from "./__root";
 
@@ -9,20 +11,35 @@ export const indexRoute = createRoute({
 
 function Home() {
     return (
-        <div className="mx-auto flex min-h-svh max-w-3xl flex-col gap-8 px-4 py-8">
-            <header className="flex flex-col gap-1">
-                <h1 className="text-3xl font-bold tracking-tight">
-                    Custom Base UI components
-                </h1>
-                <p className="text-muted-foreground">
-                    Base UI components based in shadcn/ui.
-                </p>
-            </header>
+        <div className="flex min-h-dvh w-full flex-col items-center justify-center">
+            <main className="flex flex-1 flex-col justify-center">
+                <div className="flex flex-col items-center gap-4">
+                    <div className="text-center">
+                        <h1 className="text-3xl font-bold tracking-tight">
+                            Custom Base UI components
+                        </h1>
+                        <p className="text-muted-foreground">
+                            Base UI components based in shadcn/ui.
+                        </p>
+                    </div>
 
-            <main className="flex flex-1 flex-col gap-8">
-                <p className="text-sm text-muted-foreground">
-                    No registry items yet.
-                </p>
+                    <div className="flex items-center gap-1">
+                        <Button disabled>View components</Button>
+                        <Button
+                            variant="secondary"
+                            render={
+                                <a
+                                    href="https://github.com/j-zunino/ui"
+                                    target="_blank"
+                                    rel="noreferrer"
+                                >
+                                    <IconBrandGithub />
+                                    Github
+                                </a>
+                            }
+                        />
+                    </div>
+                </div>
             </main>
         </div>
     );
